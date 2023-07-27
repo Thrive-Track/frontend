4aee5ba841e23dadd9ae6ddee83b5153f4b983a8
Owokorede <Owokoredey@gmail.com>
2023-07-27 07:08:07 +0100

    On branch sylvester Your branch is up to date with 'origin/sylvester'. Changes to be committed: modified:   index.html modified:   package-lock.json modified:   package.json new file:   public/Illustration - Scene Wireframe.png new file:   public/Key-rafiki 1.png new file:   public/Reset password-pana 1 1.png new file:   public/Resume-rafiki 1.png new file:   public/task_alt.png new file:   public/vite.svg modified:   src/App.css modified:   src/App.jsx new file:   src/Pages/Create New Password.jsx new file:   src/Pages/Create Password.css new file:   src/Pages/Icons.svg new file:   src/Pages/Loginpage.jsx new file:   src/Pages/Password Recovery.css new file:   src/Pages/Password Recovery.jsx new file:   src/Pages/SignUp.jsx new file:   src/Pages/Signuppage.css new file:   src/Pages/forgot password page.jsx new file:   src/Pages/forgot password.css new file:   src/Pages/landingpage.jsx new file:   src/Pages/login.css new file:   src/ThriveTrack App/ThriveTrackApp.jsx modified:   src/index.css modified:   src/main.jsx

---
+++ src/Pages/Loginpage.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './login.css';
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+});
+
+function Loginpage() {
+    const initialValues = {
+        email: '',
+        password: '',
+    };
+
+    const handleSubmit = (values, { setSubmitting }) => {
+        // Handle form submission here (e.g., API call, data processing, etc.)
+        console.log(values);
+        setSubmitting(false);
+    };
+
+    return (
+        <>
+            <body>
+                <div>
+                    <img src='public\Resume-rafiki 1.png'></img>
+                </div>
+                <div className='title1'>
+                    <h>Welcome Back</h>
+                </div>
+                <div>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={handleSubmit}
+                    >
+                        {({ isSubmitting }) => (
+                            <Form className='sign_in_form'>
+                                <div className='pass_email'>
+                                    <Field type='email' name='email' placeholder='Email' className='email' />
+                                    <ErrorMessage name='email' component='div' className='error_message' />
+
+                                    <Field type='password' name='password' placeholder='Password' maxLength={16} className='password' />
+                                    <ErrorMessage name='password' component='div' className='error_message' />
+                                </div>
+
+                                <button type='submit' disabled={isSubmitting} className='sign_in_btn'>
+                                    Sign in
+                                </button>
+                                <div className='other_signIn'>
+                                    <button type='submit' className='google_sign_in_btn'>
+                                        Sign in with Google
+                                    </button>
+                                    <button type='submit' className='facebook_sign_in_btn'>
+                                        Sign in with Facebook
+                                    </button>
+                                </div>
+                                <div className='forgot'>
+                                    <a className='forgot' href=''>Forgot password?</a>
+                                </div>
+
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+                <div className='go_to_sign_up'>
+                    <p>
+                        Don't have an account? <a href=''>Sign up</a>
+                    </p>
+                </div>
+            </body>
+        </>
+    );
+}
+
+export default Loginpage;

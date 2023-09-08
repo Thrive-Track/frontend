383f201c967eea27213519feed7eed3b8ff86c98
Owokorede <Owokoredey@gmail.com>
2023-09-08 12:56:26 +0100

    added some pages to the routing

--- src/main.jsx
+++ src/main.jsx
@@ -8,36 +8,6 @@ import ForgotPassword from './Pages/forgot password page.jsx';
 import PassRecover from './Pages/Password Recovery.jsx'
 import CreatePassword from './Pages/Create New Password.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/login",
-    element: <Loginpage />,
-  },
-  {
-    path: "/get started",
-    element: <Signup />,
-  },
-  {
-    path: "/sign in",
-    element: <Loginpage />,
-  },
-  {
-    path: "/sign up",
-    element: <Signup />,
-  },
-  {
-    path: "/forgot password",
-    element: <ForgotPassword />,
-  },
-]);
 
 const router = createBrowserRouter([
   {
@@ -78,10 +48,10 @@ const router = createBrowserRouter([
   }
 ]);
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 

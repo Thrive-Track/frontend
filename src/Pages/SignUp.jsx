284340f9fdcb418b81a525e93ab2989b551006c7
Owokorede <Owokoredey@gmail.com>
2023-09-08 13:13:24 +0100

    nothing......

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import './Signuppage.css';
-import { Link } from 'react-router-dom';
-
-import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "./Signuppage.css";
